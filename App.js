54754310533b31526df979a97d0b3bdce1d9a0c3
Kadri Põldmaa <kadripoldmaa@iMac.lan>
2019-11-17 13:51:33 +0200

    new font

--- App.js
+++ App.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import { StyleSheet} from 'react-native';
-
 import Navigation from './navigation/Navigator';
+import  * as Font from 'expo-font';
+import { AppLoading } from 'expo';
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'avenir-heavy': require('./assets/fonts/avenir-heavy.ttf')
+  })
+}
 
 export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  if (!fontLoaded) {
+    return <AppLoading startAsync={fetchFonts} onFinish={() => setFontLoaded(true)} />
+  }
+  
   return (
     <Navigation />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
